f572bf2f3e487ec1fd7e6c265411c9c88ebe272d
Yoga Hanggara <yohang88@gmail.com>
2019-09-08 11:54:44 +0700

    fix unit test examples
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('Card.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'Card Title'
+    const wrapper = shallowMount(Card, {
+      propsData: { title }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch(title)
+  })
+
+  it('renders with padding', () => {
+    const wrapper = shallowMount(Card, {
+      propsData: { isPadding: true }
+    })
+
+    const div = wrapper.find('.card-body')
+    expect(div.classes()).not.toContain('no-padding')
+  })
+
+  it('renders with no padding', () => {
+    const wrapper = shallowMount(Card, {
+      propsData: { isPadding: false }
+    })
+
+    const div = wrapper.find('.card-body')
+    expect(div.classes()).toContain('no-padding')
   })
 })
